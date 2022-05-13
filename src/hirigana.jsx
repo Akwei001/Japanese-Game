a063d9d0651635a667c66c41aadd11e9a9a45c30
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2022-05-13 23:11:51 +0100

    new functions added

---
+++ src/hirigana.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function hirigana() {
+  return <div>hirigana</div>;
+}
+
+export default hirigana;
---
+++ src/katakana.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function katakana() {
+  return <div>katakana</div>;
+}
+
+export default katakana;
